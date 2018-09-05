4107e8457e65e621dea6fdc300ea19f2236f9b1b
prorok9898 <43012529+prorok9898@users.noreply.github.com>
2018-09-05 21:39:45 +0300

    gggg

---
+++ bay.js
@@ -0,0 +1,34 @@
+alert("Дарова!!");
+var num = 1.45;
+console.log (num);
+var bool = true;
+console.log (bool);
+
+var name = prompt ("Как вас зовут?");
+console.log ("Вас зовут " + name);
+
+var one = 12;
+var two = 24;
+//document.write (Math.floor (2.10) +"<br>");
+//document.write ("Результат: " , one + two);
+one--;
+
+var color = ["Red", "Blue", "Green"];
+color.push ("Grey");
+console.log (color [2]);
+
+
+var  num_1 = 34;
+var  num_2 = 45;
+if(num_1 > num_2) {
+console.log (num_1 + ">" + num_2);
+} else if (num_1 == num_2) {
+console.log  (num_1 + "=" + num_2);
+} else {
+	console.log (num_1 + "<" + num_2);
+}
+
+
+
+
+
